a3205418299a4851bcee22e6b5cfb433dd14728c
Netherdrake <_@furion.me>
2021-03-23 23:51:42 +0100

    Implement rewards, winner election

--- modules/poc/src/lib.rs
+++ modules/poc/src/lib.rs
@@ -13,7 +13,7 @@ use frame_support::{
 	ensure,
 	transactional,
 };
-use frame_support::sp_runtime::traits::CheckedAdd;
+use frame_support::sp_runtime::traits::{CheckedAdd, CheckedDiv};
 use frame_system::pallet_prelude::*;
 
 #[cfg(feature = "std")]
@@ -23,6 +23,19 @@ mod mock;
 mod tests;
 
 pub use module::*;
+
+/// How many members does the technical council have?
+pub const COUNCIL_SIZE: usize = 3;
+/// How long (in block count) is the era
+pub const ERA_DURATION: u32 = 7 * primitives::time::DAYS;
+/// How many eras per year there are (roughly)?
+pub const ERAS_PER_YEAR: u32 = 52;
+/// Yearly returns: notional / YEARLY_RETURNS_DENOM
+pub const YEARLY_RETURNS_DENOM: u32 = 10;
+/// Fixed rate per-era rewards for Council members
+pub const ERA_COUNCIL_REWARDS: u32 = 100; //TODO: * primitives::currency::DOLLARS;
+
+
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq)]
@@ -63,18 +76,15 @@ impl Default for LockDuration {
 }
 
 
-// config
-// % of total supply that can be locked
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Encode, Decode, Clone, Default)]
+pub struct Era<BlockNumber> {
+	pub index: EraIndex,
+	pub start: BlockNumber,
+}
 
-// voting
-// only LockState::Committed funds can vote
-// lock duration multiplier
 
-// council
-// how does council get paid?
-// membership pallet?
-// how often are elections?
-
+pub type EraIndex = u32;
 pub type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 pub type CommitmentOf<T> =
 	Commitment<
@@ -86,6 +96,9 @@ pub type CommitmentOf<T> =
 #[frame_support::pallet]
 pub mod module {
 	use super::*;
+	// TODO: is std use allowed?
+	use std::iter::FromIterator;
+	use std::collections::BTreeMap;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -109,13 +122,37 @@ pub mod module {
 
 	#[pallet::event]
 	#[pallet::generate_deposit(fn deposit_event)]
-	#[pallet::metadata(T::AccountId = "AccountId")]
+	#[pallet::metadata(T::AccountId = "AccountId", BalanceOf<T> = "BalanceOf")]
 	pub enum Event<T: Config> {
 		/// Created a new committment
 		Committed(T::AccountId),
 		/// Add more funds to existing commitment
 		FundsAdded(T::AccountId),
+		/// Voter,Candidate,VotingPower
+		Voted(T::AccountId, T::AccountId, BalanceOf<T>),
+		/// Voter,Reward
+		VoterRewarded(EraIndex, T::AccountId, BalanceOf<T>),
 	}
+
+	#[pallet::type_value]
+	pub(super) fn FirstEra<T: Config>() -> Era<T::BlockNumber> {
+		Era {
+			index: (0 as u32).into(),
+			start: (0 as u32).into(),
+		}
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn current_era)]
+	pub(super) type CurrentEra<T: Config> =
+		StorageValue<_, Era<T::BlockNumber>, ValueQuery, FirstEra<T>>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn voter_rewards)]
+	pub(crate) type VoterRewards<T: Config> = StorageDoubleMap<_,
+		Blake2_128Concat, EraIndex,
+		Blake2_128Concat, T::AccountId, BalanceOf<T>,
+		ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn commitments)]
@@ -127,8 +164,51 @@ pub mod module {
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {
-		fn on_finalize(_n: T::BlockNumber) {
-			// TODO set winners at the end of era
+		fn on_finalize(n: T::BlockNumber) {
+			let current_era = <CurrentEra<T>>::get();
+			let era_duration: T::BlockNumber = ERA_DURATION.into();
+
+			if current_era.start + era_duration >= n {
+				// move the era forward
+				let new_era = Era{index: current_era.index, start: n};
+				<CurrentEra<T>>::set(new_era);
+
+				// clear old voter rewards (to save space)
+				<VoterRewards<T>>::remove_prefix(&current_era.index);
+
+				// set winners on new era
+				let mut counter: BTreeMap<T::AccountId, BalanceOf<T>> = BTreeMap::new();
+
+				for (_, c) in <Commitments<T>>::iter() {
+					if counter.contains_key(&c.candidate) {
+						let acc_w = *counter.get(&c.candidate).unwrap();
+						counter.insert(c.candidate.clone(), Self::voting_weight(&c) + acc_w);
+					} else {
+						counter.insert(c.candidate.clone(), Self::voting_weight(&c));
+					}
+				}
+				let mut sorted = Vec::from_iter(counter);
+				sorted.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+
+				let mut winners: Vec<T::AccountId> = Vec::new();
+				for (candidate, _weight) in sorted.iter().take(COUNCIL_SIZE) {
+					winners.push(candidate.to_owned());
+				}
+
+				// TODO: assign winners to the quorum
+
+				// distribute winners rewards
+				let zero = BalanceOf::<T>::from(0 as u32);
+				let rewards = BalanceOf::<T>::from(ERA_COUNCIL_REWARDS);
+				let reward = rewards.checked_div(&BalanceOf::<T>::from(COUNCIL_SIZE as u32)).unwrap_or(zero);
+				if reward > zero {
+					for winner in winners.iter() {
+						// ignore failed cases
+						T::Currency::deposit_into_existing(&winner, reward).ok();
+					}
+				}
+
+			}
 		}
 	}
 
@@ -146,7 +226,7 @@ pub mod module {
 
 			ensure!(!<Commitments<T>>::contains_key(&origin), Error::<T>::AlreadyCommitted);
 
-			// TODO: consider imposing a minimum bond size
+			// TODO: consider imposing a minimum bond size (to make on_finalize faster)
 			ensure!(amount >= BalanceOf::<T>::from(0 as u32), Error::<T>::InsufficientAmount);
 
 			// TODO check if at totalSupply capacity
@@ -249,6 +329,7 @@ pub mod module {
 
 
 		#[pallet::weight(10_000)]
+		#[transactional]
 		pub fn set_candidate(
 			origin: OriginFor<T>,
 			candidate: T::AccountId,
@@ -259,19 +340,34 @@ pub mod module {
 			let mut commitment = <Commitments<T>>::get(&origin);
 			ensure!(commitment.state == LockState::Committed, Error::<T>::NotCommitted);
 
-			commitment.candidate = candidate;
-			<Commitments<T>>::insert(&origin, commitment);
+			if commitment.candidate != candidate {
+				commitment.candidate = candidate.clone();
+				<Commitments<T>>::insert(&origin, &commitment);
+				Self::deposit_event(Event::Voted(origin.clone(), candidate, Self::voting_weight(&commitment)));
+			}
+
+			let apy = Self::apy_reward(&commitment);
+			let zero = BalanceOf::<T>::from(0 as u32);
+			let reward = apy.checked_div(&BalanceOf::<T>::from(ERAS_PER_YEAR)).unwrap_or(zero);
+			if reward > zero {
+				// check if nominator has been rewarded already for this era
+				let current_era = <CurrentEra<T>>::get();
+				if !<VoterRewards<T>>::contains_key(&current_era.index, &origin) {
+					<VoterRewards<T>>::insert(&current_era.index, &origin, &reward);
+					Self::deposit_event(Event::VoterRewarded(current_era.index, origin, reward));
+				}
+			}
 
 			Ok(().into())
 		}
-
-		// TODO claim reward fn
 
 
 	}
 }
 
 impl<T: Config> Pallet<T> {
+	/// Voting shares based on currently committed amount.
+	/// Monthly locks have 1x voting power, yearly 10x and 10 yearly 100x.
 	pub fn voting_weight(commitment: &Commitment<T::AccountId, BalanceOf<T>, T::BlockNumber>) -> BalanceOf<T> {
 		if commitment.state != LockState::Committed {
 			return BalanceOf::<T>::from(0 as u32);
@@ -283,4 +379,27 @@ impl<T: Config> Pallet<T> {
 		};
 		commitment.amount * BalanceOf::<T>::from(multiplier as u32)
 	}
+
+	/// Yearly reward amount based on currently committed amount.
+	/// Montly locks yield 0% APY. Yearly and 10 Year locks yield 10% APY.
+	pub fn apy_reward(commitment: &Commitment<T::AccountId, BalanceOf<T>, T::BlockNumber>) -> BalanceOf<T> {
+		let zero = BalanceOf::<T>::from(0 as u32);
+
+		if commitment.state != LockState::Committed {
+			return zero;
+		}
+
+		match commitment.duration {
+			LockDuration::OneMonth => {
+				zero
+			},
+			_ => {
+				commitment.amount
+					.checked_div(&BalanceOf::<T>::from(YEARLY_RETURNS_DENOM))
+					.unwrap_or(zero)
+			}
+		}
+	}
+
+
 }
