d4a84956a2bab2d52eab5523bcc8a2fc4bc35542
Furion <_@furion.me>
2021-03-23 09:37:40 +0100

    Gas fixes
    Backport fix for #794

--- runtime/common/src/precompile/tests.rs
+++ runtime/common/src/precompile/tests.rs
@@ -202,7 +202,7 @@ fn schedule_call_precompile_should_work() {
 		run_to_block(5);
 		#[cfg(not(feature = "with-ethereum-compatibility"))]
 		{
-			assert_eq!(Balances::free_balance(from_account.clone()), 999999995255);
+			assert_eq!(Balances::free_balance(from_account.clone()), 999999905647);
 			assert_eq!(Balances::reserved_balance(from_account), 0);
 			assert_eq!(Balances::free_balance(to_account), 1000000001000);
 		}
@@ -286,7 +286,7 @@ fn schedule_call_precompile_should_handle_invalid_input() {
 		);
 
 		run_to_block(4);
-		assert_eq!(Balances::free_balance(from_account.clone()), 999999999954);
+		assert_eq!(Balances::free_balance(from_account.clone()), 999999910346);
 		assert_eq!(Balances::reserved_balance(from_account), 0);
 		assert_eq!(Balances::free_balance(to_account), 1000000000000);
 	});
