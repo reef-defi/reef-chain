011d4f4d9d62580bfb2b2516c484956f975087d0
Netherdrake <_@furion.me>
2021-04-26 01:09:43 +0200

    EDA: offline validators

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -1,17 +1,17 @@
 from substrateinterface import SubstrateInterface
 
 import json
-types = json.load(open("../types.json", "r"))
+types = json.load(open("../assets/types.json", "r"))
 
-ip = "127.0.0.1"
-port = 9933
-
+# url = "http://127.0.0.1:9933"
+url = "https://rpc-testnet.reefscan.com"
+# url = "https://rpc.reefscan.com"
 
 s = SubstrateInterface(
-    url=f"http://{ip}:{port}",
+    url=url,
     ss58_format=42,
     type_registry=types,
-    # type_registry_preset='substrate-node-template'
+    type_registry_preset='substrate-node-template'
 )
 
 s.chain, s.version, s.properties
@@ -24,6 +24,9 @@ s.get_chain_head()
 
 ## totalSupply
 s.query('Balances', 'TotalIssuance')
+
+## current metadata
+s.get_runtime_metadata()
 
 ## send 100 REEF from Alice -> Bob
 from substrateinterface import Keypair
@@ -39,9 +42,8 @@ call = s.compose_call(
     }
 )
 extrinsic = s.create_signed_extrinsic(call=call, keypair=alice)
-# dry run
+# tx dry run
 s.get_payment_info(call=call, keypair=alice)
-
 
 
 ## check tx metadata
@@ -52,3 +54,36 @@ receipt = ExtrinsicReceipt(
     block_hash="0x3cdc5e4e145a25d0361c66669a062695d0e2a511cdd6d4868cfda80e66cf185c"
 )
 receipt.is_success, receipt.weight, receipt.total_fee_amount
+
+## find validators missing blocks
+
+# get current validators
+validators = [s.ss58_encode(x) for x in s.query('Session', 'Validators').value]
+validators
+
+# get current head
+r = s.get_runtime_block(include_author=True)
+r
+author = r['block']['author']
+parent = r['block']['header']['parentHash']
+
+from tqdm import tqdm
+active_validators = []
+for _ in tqdm(range(0, 100)):
+    r = s.get_runtime_block(block_hash=parent, include_author=True)
+    author = r['block']['author']
+    parent = r['block']['header']['parentHash']
+    active_validators.append(author)
+
+
+# recently stopped validating or offline
+set(active_validators) ^ set(validators)
+
+# offline validators
+set(validators) - set(active_validators)
+
+# online validator statistics
+import collections
+c = collections.Counter(active_validators)
+c
+
