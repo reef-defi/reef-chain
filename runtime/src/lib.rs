6819fa77d8f72671bb8725905ede13616d615d24
Furion <_@furion.me>
2021-04-28 01:44:05 +0200

    Increase validator rewards

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -344,14 +344,14 @@ impl pallet_session::historical::Config for Runtime {
 }
 
 pallet_staking_reward_curve::build! {
-	// 2.5% min, 22.5% max, 50% ideal stake
+	// 3.5% min, 27.5% max, 50% ideal stake
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-		min_inflation: 0_025_000,
-		max_inflation: 0_225_000,
+		min_inflation: 0_035_000,
+		max_inflation: 0_275_000,
 		ideal_stake: 0_500_000,
 		falloff: 0_050_000,
 		max_piece_count: 40,
-		test_precision: 0_005_000,
+		test_precision: 0_008_000,
 	);
 }
 
