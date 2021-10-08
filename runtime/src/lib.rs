a00ea2e8209f4af55d3152aa14dff47567ea224d
frisitano <francesco.risitano95@gmail.com>
2021-10-08 13:56:55 +0100

    bump reef to v8

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -231,8 +231,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("reef"),
 	impl_name: create_runtime_str!("reef"),
 	authoring_version: 1,
-	spec_version: 7,
-	impl_version: 7,
+	spec_version: 8,
+	impl_version: 8,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 };
